0ea201d296fde04e9629ac979e2966e5eda49622
Unknown <yoyo45_ds@hotmail.fr>
2017-02-28 14:14:32 +0100

    player jumping

---
+++ Assets/YoanTestDirectory/FishJumping.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(FishMoving))]
+[RequireComponent(typeof(Rigidbody))]
+public class FishJumping : MonoBehaviour {
+
+    public float _raycastMaxRange;
+    public float _multiplyBonusForJump;
+
+    private bool _ascend;
+    private bool _onJump;
+    private float _lastPosY;
+    private Rigidbody _rigid;
+    private FishMoving _fishMoving;
+
+    private void Awake()
+    {
+        _rigid = GetComponentInParent<Rigidbody>();
+        _fishMoving = GetComponentInParent<FishMoving>();
+    }
+
+    // Use this for initialization
+    void Start () {
+        _ascend = false;
+        _lastPosY = transform.position.y;
+        _onJump = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (_lastPosY < transform.position.y)
+        {
+            _ascend = true;
+        }
+        else
+        {
+            _ascend = false;
+        }
+        _lastPosY = transform.position.y;
+        if (_ascend && !_onJump && !Physics.Raycast(transform.position, -Vector3.right, _raycastMaxRange))
+        {
+            _rigid.AddRelativeForce(Vector3.up * _fishMoving._movementSpeed * _multiplyBonusForJump);
+            _onJump = true;
+        }
+        
+	}
+}
--- Assets/YoanTestDirectory/FishMoving.cs
+++ Assets/YoanTestDirectory/FishMoving.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class Move : MonoBehaviour {
+public class FishMoving : MonoBehaviour {
 
-    public float _speed;
+    public float _movementSpeed;
 
     private Rigidbody _rigid;
 
@@ -16,6 +16,6 @@ public class Move : MonoBehaviour {
 	// Update is called once per frame
 	private void Update () {
         _rigid.velocity = new Vector3(0,_rigid.velocity.y,0);
-        _rigid.AddRelativeForce(-Vector3.right * _speed);
+        _rigid.AddRelativeForce(-Vector3.right * _movementSpeed);
 	}
 }
