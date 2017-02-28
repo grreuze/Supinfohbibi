3b5d00676b9a1484cca0723f811b5809e7942689
ETU\g.meyer <g.meyer@rubika-edu.com>
2017-02-28 14:01:16 +0100

    Added chunk generation + organized folder struct
    Chunk generation system that includes angles (if you have trouble with it ask greg)

---
+++ Assets/Scripts/ChunkGeneration/ChunkScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkScript : MonoBehaviour {
+
+    //la fin du chunk, qui détermine l'endroit et la rotation du prochain chunk instancié
+    public Transform EndPoint;
+
+    [HideInInspector]
+    public float chunkLength;
+
+    private void Start()
+    {
+        CalculateChunkLength();
+        Calibrate();
+    }
+
+    public void CalculateChunkLength()
+    {
+        //on crée 2 nouveaux vecteurs avec les positions de départ et d'arrivée mais au même niveau en Y.
+        Vector3 FlatStart = new Vector3(transform.position.x, 0, transform.position.z);
+        Vector3 FlatEnd = new Vector3(EndPoint.position.x, 0, EndPoint.position.z);
+
+        //on calcule la distance "flat" entre les 2 points
+        chunkLength = Vector3.Distance(FlatStart, FlatEnd);
+    }
+
+    //calibration du LevelGeneration (mise à jour des variables)
+    public void Calibrate()
+    {
+        LevelGeneration.ins.CurrentEndPoint = EndPoint;
+        LevelGeneration.ins.currentLength = chunkLength;
+    }
+}
---
+++ Assets/Scripts/ChunkGeneration/LevelGeneration.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour {
+
+    public static LevelGeneration ins;
+
+    //l'array de chunks
+    public GameObject[] Chunks;
+
+    [HideInInspector]
+    public Transform CurrentEndPoint;
+
+    [HideInInspector]
+    public float currentLength;
+
+    //INTS
+    //nombre de chunks instanciés
+    private int chunkScore;
+    //le chunk précédemment instancié (pour éviter les doublons)
+    private int previousChunkIndex;
+    //l'int random pour pick le chunk a instancier
+    private int chunkIndexer;
+
+    //Singleton
+    private void Awake()
+    {
+        if(LevelGeneration.ins == null)
+        {
+            LevelGeneration.ins = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Generate()
+    {
+        //on interdit les doublons de chunks via un do/while
+        do
+        {
+            chunkIndexer = Random.Range(0, Chunks.Length);
+        } while (CheckChunkValidity(chunkIndexer));
+
+        previousChunkIndex = chunkIndexer;
+
+        //Instanciation du préfab
+        GameObject newChunk = Instantiate(Chunks[chunkIndexer], CurrentEndPoint.position, Quaternion.Euler(CurrentEndPoint.eulerAngles.x, CurrentEndPoint.eulerAngles.y, CurrentEndPoint.eulerAngles.z));       
+
+        chunkScore++;
+    }
+
+    //vérifie que le random est bien valide (avec exception au tout début)
+    bool CheckChunkValidity(int ind)
+    {
+        if (chunkScore == 0)
+            return false;
+
+        if (ind != previousChunkIndex)
+            return false;
+        else return true;
+    }
+}
