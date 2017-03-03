12db72d7ff537d52170e4a02c38c94b3d7c9b1ec
Unknown <loicanquez@gmail.com>
2017-03-03 11:21:56 +0100

    Score Fin de Race
    +Debug Points etc

--- Assets/YoanTestDirectory/GameManager.cs
+++ Assets/YoanTestDirectory/GameManager.cs
@@ -1,62 +1,68 @@
 ﻿using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
-    public static GameManager instance;
-    [SerializeField]
-    bool playAuto;
+	public static GameManager instance;
+	[SerializeField]
+	bool playAuto;
 
-    private void Awake() {
-        if (instance != null && instance != this) {
-            Destroy(this.gameObject);
-        } else {
-            instance = this;
-            DontDestroyOnLoad(this.gameObject);
-        }
-    }
+	private void Awake ()
+	{
+		if (instance != null && instance != this) {
+			Destroy (this.gameObject);
+		} else {
+			instance = this;
+			DontDestroyOnLoad (this.gameObject);
+		}
+	}
 
-    void Start() {
-        if (playAuto)
-            SpawnFishes();
-    }
+	void Start ()
+	{
+		if (playAuto)
+			SpawnFishes ();
+	}
 
-    public static GameManager GetInstance() {
-        return instance;
-    }
+	public static GameManager GetInstance ()
+	{
+		return instance;
+	}
 
-    public bool isPlaying = false;
-    public float _playerStartMoveSpeed;
-    public float _maxMoveSpeed;
-    public float _minMoveSpeed;
-    public int _nbOpponent;
-    public float _maxOpponentSpeed;
-    public GameObject _fish;
-    public GameObject aiFish;
-    public Canvas _endCanvas;
+	public bool isPlaying = false;
+	public float _playerStartMoveSpeed;
+	public float _maxMoveSpeed;
+	public float _minMoveSpeed;
+	public int _nbOpponent;
+	public float _maxOpponentSpeed;
+	public GameObject _fish;
+	public GameObject aiFish;
+	public Canvas _endCanvas;
 
-    public void SpawnFishes() {
-        Instantiate(_fish, transform.position, transform.rotation);
-        for (int i = 0; i < _nbOpponent; i++) {
-            Vector3 spawnPosition = transform.position;
-            spawnPosition.x += Random.Range(-2, 2);
-            spawnPosition.z += Random.Range(-2, 2);
+	public void SpawnFishes ()
+	{
+		Instantiate (_fish, transform.position, transform.rotation);
+		for (int i = 0; i < _nbOpponent; i++) {
+			Vector3 spawnPosition = transform.position;
+			spawnPosition.x += Random.Range (-2, 2);
+			spawnPosition.z += Random.Range (-2, 2);
 
-            GameObject go = Instantiate(aiFish, spawnPosition, transform.rotation);
-            go.GetComponentInParent<AIFish>().movementSpeed = Random.Range(_minMoveSpeed, _maxMoveSpeed);
+			GameObject go = Instantiate (aiFish, spawnPosition, transform.rotation);
+			go.GetComponentInParent<AIFish> ().movementSpeed = Random.Range (_minMoveSpeed, _maxMoveSpeed);
 
-            //go.GetComponentInParent<AIFish>().SetSpeed(_minMoveSpeed + ((_maxOpponentSpeed - _minMoveSpeed) / _nbOpponent) * i);
-        }
-        isPlaying = true;
-        StartRun();
-    }
+			//go.GetComponentInParent<AIFish>().SetSpeed(_minMoveSpeed + ((_maxOpponentSpeed - _minMoveSpeed) / _nbOpponent) * i);
+		}
+		isPlaying = true;
+		StartRun ();
+	}
 
-    public void EndRun()
-    {
-        _endCanvas.enabled = true;
-    }
+	public void EndRun ()
+	{
+		_endCanvas.enabled = true;
+		_endCanvas.GetComponentInChildren<Score> ().ScoreFinal ();
+	}
 
-    public void StartRun()
-    {
-        _endCanvas.enabled = false;
-    }
+	public void StartRun ()
+	{
+		_endCanvas.enabled = false;
+	}
 }
