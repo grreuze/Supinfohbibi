28afa17f81e1489f06026e19fde7dfb70afdd91d
Unknown <yoyo45_ds@hotmail.fr>
2017-03-03 12:10:37 +0100

    rotate with Trick

--- Assets/FishAnimator.cs
+++ Assets/FishAnimator.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class FishAnimator : MonoBehaviour {
 
-    Animator anim;
+    private Animator anim;
+    private bool faceCam;
 
 	// Use this for initialization
 	void Start () {
         anim = GetComponentInParent<Animator>();
+        faceCam = false;
 	}
 	
 	public void SetGrounded(bool newValue)
@@ -23,12 +25,22 @@ public class FishAnimator : MonoBehaviour {
 
     public void SetTrick()
     {
+        if (!faceCam)
+        {
+            faceCam = true;
+            transform.Rotate(new Vector3(0, 180, 0));
+        }
         anim.SetInteger("Trick_Indexer", Random.Range(1, 10));
         anim.SetTrigger("Trick_Trigger");
     }
 
     public void SetEndOfTrick()
     {
+        if(faceCam)
+        {
+            faceCam = false;
+            transform.Rotate(new Vector3(0, -180, 0));
+        }
         anim.SetTrigger("Trick_End");
     }
 }
