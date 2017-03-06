f1342442e1c9a5a4a63eb6702cfb8df9acff5521
grreuze <grreuze@gmail.com>
2017-03-06 16:30:52 +0100

    fixed empty namespace calls

--- Assets/YoanTestDirectory/RestartScript.cs
+++ Assets/YoanTestDirectory/RestartScript.cs
@@ -1,11 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using DG.Tweening;
 
-public class RestartScript : MonoBehaviour
-{
+public class RestartScript : MonoBehaviour {
 
 	public GameObject buttonMenu;
 	public GameObject buttonRestart;
@@ -19,15 +16,8 @@ public class RestartScript : MonoBehaviour
 				GameManager.instance._endCanvas = canvas;
 			}
 		}
-
 		buttonMenu.transform.DOScale (new Vector3 (1.05f, 1.05f, 1.05f), 0.4f).SetLoops (-1, LoopType.Yoyo);
 		buttonRestart.transform.DOScale (new Vector3 (1.05f, 1.05f, 1.05f), 0.4f).SetLoops (-1, LoopType.Yoyo);
-	}
-
-	// Update is called once per frame
-	void Update ()
-	{
-		
 	}
 
 	public void ReloadScene ()
