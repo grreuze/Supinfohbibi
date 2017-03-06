a8e56eaa3591101e084774056bfc5aa405f7cc61
grreuze <grreuze@gmail.com>
2017-03-06 09:27:56 +0100

    Réorganisation du dossier

---
+++ Assets/Scripts/Loading.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+	public Image bar;
+
+	IEnumerator Start ()
+	{
+		AsyncOperation async = Application.LoadLevelAsync ("GrrGeneration");
+		bar.fillAmount = async.progress;
+		yield return async;
+
+
+	}
+}
