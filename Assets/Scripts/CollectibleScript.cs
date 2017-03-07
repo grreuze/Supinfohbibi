c032d75ded62a956f8ec9f2a36be59125b9a426c
Unknown <loicanquez@gmail.com>
2017-03-07 17:29:27 +0100

    Sound Cut + Integration

--- Assets/Scripts/CollectibleScript.cs
+++ Assets/Scripts/CollectibleScript.cs
@@ -1,101 +1,104 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CollectibleScript : MonoBehaviour {
+public class CollectibleScript : MonoBehaviour
+{
 
-    [Header("Gros boost")]
-    [Range(1, 2)]
-    public float boostValue;
-    public float boostTime;
+	[Header ("Gros boost")]
+	[Range (1, 2)]
+	public float boostValue;
+	public float boostTime;
 
-    [Header("Petit Boost")]
-    [Range(1, 1.5f)]
-    public float smallBoostValue;
-    public float smallBoostTime;
+	[Header ("Petit Boost")]
+	[Range (1, 1.5f)]
+	public float smallBoostValue;
+	public float smallBoostTime;
 
-    float originalSpeed;
+	float originalSpeed;
 
-    static int combo = 0;
+	static int combo = 0;
 
-    ParticleSystem CollectedEffect;
+	ParticleSystem CollectedEffect;
 
-    private void Start() {
-        originalSpeed = GameManager.GetInstance()._maxMoveSpeed;
-        CollectedEffect = GetComponentInChildren<ParticleSystem>();
-        originalSpeed = GameManager.GetInstance()._maxMoveSpeed;
-    }
+	private void Start ()
+	{
+		originalSpeed = GameManager.GetInstance ()._maxMoveSpeed;
+		CollectedEffect = GetComponentInChildren<ParticleSystem> ();
+		originalSpeed = GameManager.GetInstance ()._maxMoveSpeed;
+	}
 
-    private void FixedUpdate() {
-        transform.eulerAngles += new Vector3(0, 5, 0);
-    }
+	private void FixedUpdate ()
+	{
+		transform.eulerAngles += new Vector3 (0, 5, 0);
+	}
 
-    private void ResetCombo() {
-        CollectibleScript.combo = 0;
-    }
+	private void ResetCombo ()
+	{
+		CollectibleScript.combo = 0;
+	}
 
-    bool alreadyCalled;
-    private void OnTriggerEnter(Collider collision)
-    {
-        //si le joueur est un player
-        if(collision.gameObject.tag == "Player")
-        {
+	bool alreadyCalled;
 
-            if(!alreadyCalled) {
-                GameManager.GetInstance().coins++;
-                alreadyCalled = true;
-            }
+	private void OnTriggerEnter (Collider collision)
+	{
+		//si le joueur est un player
+		if (collision.gameObject.tag == "Player") {
 
-            //Lecture du son de pickup
-            AudioManager.ins.PlayCollectibleSound(combo, gameObject);
+			if (!alreadyCalled) {
+				GameManager.GetInstance ().coins++;
+				AudioManager.ins.PlayCollectibleSound (combo, gameObject);
+				alreadyCalled = true;
+			}
 
-            //Si le combo est en cours
-            if(CollectibleScript.combo < 5)
-            {
-                CancelInvoke();
+			//Lecture du son de pickup
+			AudioManager.ins.PlayCollectibleSound (combo, gameObject);
+
+			//Si le combo est en cours
+			if (CollectibleScript.combo < 5) {
+				CancelInvoke ();
                 
-                CollectibleScript.combo++;
-                Invoke("ResetCombo", 0.5f);
+				CollectibleScript.combo++;
+				Invoke ("ResetCombo", 0.5f);
 
-                //reset des coroutines et de la vitesse pour éviter le stack de coroutines
-                StopCoroutine(SmallBoost());
-                GameManager.GetInstance()._maxMoveSpeed = originalSpeed;
+				//reset des coroutines et de la vitesse pour éviter le stack de coroutines
+				StopCoroutine (SmallBoost ());
+				GameManager.GetInstance ()._maxMoveSpeed = originalSpeed;
 
-                StartCoroutine(SmallBoost());
+				StartCoroutine (SmallBoost ());
 
-                AudioManager.ins.PlayCollectibleSound(combo, gameObject);
-            }
-            else //si le combo est complété
-            {
-                StartCoroutine(Boost());
-                ResetCombo();
-            }
+                
+			} else { //si le combo est complété
+				StartCoroutine (Boost ());
+				ResetCombo ();
+			}
 
-            CollectedEffect.Emit(1);
-            GetComponent<Renderer>().enabled = false;
-            Invoke("Disable", CollectedEffect.main.duration);
-        }
-    }
+			CollectedEffect.Emit (1);
+			GetComponent<Renderer> ().enabled = false;
+			Invoke ("Disable", CollectedEffect.main.duration);
+		}
+	}
 
-    void Disable() {
-        gameObject.SetActive(false);
-    }
+	void Disable ()
+	{
+		gameObject.SetActive (false);
+	}
 
-    private IEnumerator Boost()
-    {
-        GameManager.GetInstance()._maxMoveSpeed *= boostValue;
+	private IEnumerator Boost ()
+	{
+		GameManager.GetInstance ()._maxMoveSpeed *= boostValue;
 
-        yield return new WaitForSeconds(boostTime);
+		yield return new WaitForSeconds (boostTime);
 
-        GameManager.GetInstance()._maxMoveSpeed = originalSpeed;
-    }
+		GameManager.GetInstance ()._maxMoveSpeed = originalSpeed;
+	}
 
-    private IEnumerator SmallBoost()
-    {
-        GameManager.GetInstance()._maxMoveSpeed *= smallBoostValue;
+	private IEnumerator SmallBoost ()
+	{
+		GameManager.GetInstance ()._maxMoveSpeed *= smallBoostValue;
 
-        yield return new WaitForSeconds(smallBoostTime);
+		yield return new WaitForSeconds (smallBoostTime);
 
-        GameManager.GetInstance()._maxMoveSpeed = originalSpeed;
-    }
+		GameManager.GetInstance ()._maxMoveSpeed = originalSpeed;
+	}
 
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,117 +1,127 @@
 ﻿using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
-    public static GameManager instance;
-    [SerializeField]
-    bool playAuto;
+	public static GameManager instance;
+	[SerializeField]
+	bool playAuto;
 
-    private void Awake() {
-        if (instance != null && instance != this) {
-            Destroy(gameObject);
-        } else {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-    }
+	private void Awake ()
+	{
+		if (instance != null && instance != this) {
+			Destroy (gameObject);
+		} else {
+			instance = this;
+			DontDestroyOnLoad (gameObject);
+		}
+	}
 
-    private BestScore[] bestScoreText;
+	private BestScore[] bestScoreText;
 
-    void Start() {
-        Screen.orientation = ScreenOrientation.Portrait;
-        endRun = GetComponent<EndRun>();
-        Metrics.ins.Setup();
-        endRun.OnRunStart();
-        trickSystem = FindObjectOfType<Trick_Pattern>();
-        if (playAuto)
-            SpawnFishes();
-        bestScore = endRun.PlayerStats.bestScore;
-        UpdateBestScore();
-    }
+	void Start ()
+	{
+		Screen.orientation = ScreenOrientation.Portrait;
+		endRun = GetComponent<EndRun> ();
+		Metrics.ins.Setup ();
+		endRun.OnRunStart ();
+		trickSystem = FindObjectOfType<Trick_Pattern> ();
+		if (playAuto)
+			SpawnFishes ();
+		bestScore = endRun.PlayerStats.bestScore;
+		UpdateBestScore ();
+	}
 
-    public static GameManager GetInstance() {
-        return instance;
-    }
+	public static GameManager GetInstance ()
+	{
+		return instance;
+	}
 
-    public bool isPlaying = false;
-    public float baseMoveSpeed;
-    public float accelerateMoveSpeed;
-    public float decelerateMoveSpeed;
-    public float JumpForce;
-    public float _playerStartMoveSpeed;
-    public float _maxMoveSpeed;
-    public float _minMoveSpeed;
-    public int _nbOpponent;
-    public float _maxOpponentSpeed;
-    public GameObject _fish;
-    public GameObject aiFish;
-    public Canvas _endCanvas;
-    public bool soundPause = false;
-    public int coins;
+	public bool isPlaying = false;
+	public float baseMoveSpeed;
+	public float accelerateMoveSpeed;
+	public float decelerateMoveSpeed;
+	public float JumpForce;
+	public float _playerStartMoveSpeed;
+	public float _maxMoveSpeed;
+	public float _minMoveSpeed;
+	public int _nbOpponent;
+	public float _maxOpponentSpeed;
+	public GameObject _fish;
+	public GameObject aiFish;
+	public Canvas _endCanvas;
+	public bool soundPause = false;
+	public int coins;
 
-    private Trick_Pattern trickSystem;
-    private int bestScore = 0;
-    private EndRun endRun;
+	private Trick_Pattern trickSystem;
+	private int bestScore = 0;
+	private EndRun endRun;
 
-    public void UpdateBestScore() {
-        bestScoreText = FindObjectsOfType<BestScore>();
-        foreach (BestScore best in bestScoreText) {
-            best.UpdateBestScore(bestScore);
-        }
-    }
+	public void UpdateBestScore ()
+	{
+		bestScoreText = FindObjectsOfType<BestScore> ();
+		foreach (BestScore best in bestScoreText) {
+			best.UpdateBestScore (bestScore);
+		}
+	}
 
-    public void SpawnFishes() {
-        Instantiate(_fish, transform.position, transform.rotation);
-        for (int i = 0; i < _nbOpponent; i++) {
-            Vector3 spawnPosition = transform.position;
-            spawnPosition.x += Random.Range(-2, 2);
-            spawnPosition.z += Random.Range(-2, 2);
+	public void SpawnFishes ()
+	{
+		Instantiate (_fish, transform.position, transform.rotation);
+		for (int i = 0; i < _nbOpponent; i++) {
+			Vector3 spawnPosition = transform.position;
+			spawnPosition.x += Random.Range (-2, 2);
+			spawnPosition.z += Random.Range (-2, 2);
 
-            GameObject go = Instantiate(aiFish, spawnPosition, transform.rotation);
-            //go.GetComponentInParent<AIFish>().movementSpeed = Random.Range(_minMoveSpeed, _maxMoveSpeed);
-        }
-        isPlaying = true;
-        StartRun();
-        Metrics.ins.StartRun();
-    }
+			GameObject go = Instantiate (aiFish, spawnPosition, transform.rotation);
+			//go.GetComponentInParent<AIFish>().movementSpeed = Random.Range(_minMoveSpeed, _maxMoveSpeed);
+		}
+		isPlaying = true;
+		StartRun ();
+		Metrics.ins.StartRun ();
+	}
 
-    public void EndRun()
-    {
-        if(!trickSystem)
-            trickSystem = FindObjectOfType<Trick_Pattern>();
-        trickSystem.EndOfTrick(); 
+	public void EndRun ()
+	{
+		if (!trickSystem)
+			trickSystem = FindObjectOfType<Trick_Pattern> ();
+		trickSystem.EndOfTrick (); 
         
-        GameObject.Find("HUD").GetComponent<Canvas>().enabled = false;
+		GameObject.Find ("HUD").GetComponent<Canvas> ().enabled = false;
 
-        CameraScript camera = Camera.main.GetComponent<CameraScript>();
-        camera.SetNewState(camera.end);
+		/*AudioManager.ins.StopSound (GameObject.Find ("Music"));
+		AudioManager.ins.PlaySoundAtPosition ("End", GameObject.Find ("Music"), true);*/
+
+		AudioManager.ins.StopSound (GameObject.Find ("WaterSound"));
+		AudioManager.ins.PlaySoundAtPosition ("LittleFlow", GameObject.Find ("WaterSound"), true);
+
+
+		CameraScript camera = Camera.main.GetComponent<CameraScript> ();
+		camera.SetNewState (camera.end);
         
-        _endCanvas.enabled = true;
-        _endCanvas.GetComponentInChildren<Score>().ScoreFinal();
-        endRun.LaunchEndRunProtocol();
-    }
+		_endCanvas.enabled = true;
+		_endCanvas.GetComponentInChildren<Score> ().ScoreFinal ();
+		endRun.LaunchEndRunProtocol ();
+	}
 
-    public void StartRun()
-    {
-        _endCanvas.enabled = false;
-    }
+	public void StartRun ()
+	{
+		_endCanvas.enabled = false;
+	}
 
-    public bool IsScoreBetterThanBest(int score)
-    {
-        if(score > bestScore)
-        {
-            bestScore = score;
-            UpdateBestScore();
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+	public bool IsScoreBetterThanBest (int score)
+	{
+		if (score > bestScore) {
+			bestScore = score;
+			UpdateBestScore ();
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    public int GetBestScore()
-    {
-        return bestScore;
-    }
+	public int GetBestScore ()
+	{
+		return bestScore;
+	}
 }
