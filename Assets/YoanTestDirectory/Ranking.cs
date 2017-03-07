bed68815b1cc591dd3d19482532dfd76c0d8cce9
Unknown <loicanquez@gmail.com>
2017-03-07 14:39:38 +0100

    Spin of the rank + lerp color

--- Assets/YoanTestDirectory/Ranking.cs
+++ Assets/YoanTestDirectory/Ranking.cs
@@ -1,38 +1,58 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class Ranking : MonoBehaviour {
+public class Ranking : MonoBehaviour
+{
 
-    private FishController fish;
-    private AIFish[] _opponentList;
-    private int _rank = 0;
-    Text display;
+	private FishController fish;
+	private AIFish[] _opponentList;
+	private int _rank = 0;
+	private int lastRank = 0;
+	GameObject display;
 
-    private void Awake()
-    {
-        fish = GetComponent<FishController>();
-        display = GameObject.Find("Rank").GetComponent<Text>();
-    }
+	public Color lastColor;
+	public Color firstColor;
 
-    // Update is called once per frame
-    void Update () {
-        _opponentList = FindObjectsOfType<AIFish>();
-        if (_opponentList.Length != 0)
-        {
-            _rank = 1;
-            for (int i = 0; i < _opponentList.Length; i++)
-            {
-                if (_opponentList[i].GetAverageSpeed() > fish.GetAverageSpeed())
-                {
-                    _rank++;
-                }
-            }
-            display.text = _rank.ToString(); ;
-        }
-    }
+	private void Awake ()
+	{
+		fish = GetComponent<FishController> ();
+		display = GameObject.Find ("Rank");
+	}
+		
 
-    public int GetRank()
-    {
-        return _rank;
-    }
+	// Update is called once per frame
+	void Update ()
+	{
+		_opponentList = FindObjectsOfType<AIFish> ();
+		if (_opponentList.Length != 0) {
+			_rank = 1;
+			for (int i = 0; i < _opponentList.Length; i++) {
+				if (_opponentList [i].GetAverageSpeed () > fish.GetAverageSpeed ()) {
+					_rank++;
+				}
+			}
+
+		}
+
+		if (_rank != lastRank) {
+			display.GetComponent<Text> ().text = _rank.ToString ();
+			display.GetComponent<Text> ().color = Color.Lerp (firstColor, lastColor, (float)(((float)_rank - 1f) / ((float)GameManager.GetInstance ()._nbOpponent + 1f)));
+			print ((_rank - 1) / (GameManager.GetInstance ()._nbOpponent + 1));
+			print (_rank);
+			display.GetComponent<Animation> ().Play ("RankAnim");
+			lastRank = _rank;
+
+		}
+
+
+
+	}
+
+
+
+
+	public int GetRank ()
+	{
+		return _rank;
+	}
 }
