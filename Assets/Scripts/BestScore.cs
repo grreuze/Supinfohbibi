cfe60e1c9fbf1bb439c8b6909adbc1ec98e3a5f6
Unknown <yoyo45_ds@hotmail.fr>
2017-03-08 15:34:01 +0100

    saving player settings

--- Assets/Scripts/BestScore.cs
+++ Assets/Scripts/BestScore.cs
@@ -1,25 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class BestScore : MonoBehaviour
-{
-
-	[SerializeField]
-	private Text _bestScoreText;
-
-	public void Start ()
-	{
-		_bestScoreText.text = GameManager.GetInstance ().coins.ToString ();
-	}
-
-
-
-
-	public void UpdateBestScore (int newBestScore)
-	{
-		_bestScoreText.text = newBestScore.ToString ();
-	}
-}
 
 public class BestScore : MonoBehaviour {
 
